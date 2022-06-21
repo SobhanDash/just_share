4d877ad25d5b967256490a1ab1916f53e87be861
Tushar Ranjan Nayak <tusharnayak1515@gmail.com>
2022-06-21 22:39:27 +0530

    log

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ const commentRoutes = require("./routes/comment");
 const messageRoutes = require("./routes/message");
 const connectToMongo = require("./db");
 const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
 const port = process.env.PORT || 5000;
 
 connectToMongo();
@@ -24,6 +26,64 @@ app.use("/api/auth", authRoutes);
 app.use("/api/posts", postRoutes);
 app.use("/api/comments", commentRoutes);
 app.use("/api/message", messageRoutes);
+
+let users = [];
+
+console.log("connected!");
+
+const addUser = (userId,socketId)=> {
+    // console.log(users.length);
+    !users.some((user) => user.userId === userId) &&
+    users.push({ userId, socketId});
+    // console.log(users);
+}
+
+const removeUser = (socketId)=> {
+    users = users.filter((user)=> user.socketId !== socketId);
+}
+
+const getUser = (userId)=> {
+    return users.find((user)=> user.userId === userId);
+}
+
+io.on("connection", (socket) => {
+    console.log(`A user with socket id ${socket.id} is connected!`);
+
+    // Take userId and socketId from user
+    socket.on("addUser", (userId)=> {
+        addUser(userId, socket.id);
+        console.log(users);
+        let myusers = [];
+        for(let i=0; i<users.length; i++) {
+            myusers.push(users[i].userId);
+        }
+        io.emit("getUsers", myusers);
+    });
+
+    // send and get message
+    socket.on("sendMessage", ({_id,conversation,sender,receiver,text,createdAt,updatedAt})=> {
+        let myreceiver = getUser(receiver._id);
+        // console.log(receiver._id);
+        if(myreceiver) {
+            io.to(myreceiver.socketId).emit("getMessage", {
+                _id,
+                conversation,
+                sender,
+                receiver,
+                text,
+                createdAt,
+                updatedAt
+            });
+        }
+    });
+
+    // Disconnection
+    socket.on("disconnect", ()=> {
+        console.log("disconnected");
+        removeUser(socket.id);
+        io.emit("getUsers", users);
+    })
+});
 
 if (process.env.NODE_ENV == "production") {
   app.use(express.static("client/build"));
